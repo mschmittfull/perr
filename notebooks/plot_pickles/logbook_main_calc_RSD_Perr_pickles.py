f83f2c55f9a209989d61a7582605d4f253baac4b
Marcel Schmittfull <mschmittfull@gmail.com>
2020-11-24 21:45:55 -0500

    add notebooks

---
+++ notebooks/plot_pickles/logbook_main_calc_RSD_Perr_pickles.py
@@ -0,0 +1,19 @@
+"""
+Log book of pickles saved to disk
+"""
+
+from __future__ import print_function, division
+
+
+def get_pickle_fname():
+
+    plot_opts = {}
+
+    # Can use this as a logbook to load different runs from disk
+    fname = 'main_calc_Perr_2020_Sep_03_09:14:30_time1599124470.dill'
+
+
+
+    loginfo = {'fname': fname, 'plot_opts': plot_opts}
+
+    return loginfo
